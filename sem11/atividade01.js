5bf87649392bacfacecbcb4d93bd00b7989184ff
Joao <joaopaaulomedeiros2021@gmail.com>
2025-11-21 16:07:58 -0300

    alteracao em var

--- sem11/atividade01.js
+++ sem11/atividade01.js
@@ -1,8 +1,8 @@
-var idade = Number(prompt("insira sua idade atual: "))
+var idd = Number(prompt("insira sua idade atual: "))
 
 function converterIdadeEmDias(idadeEmDias) {
-    var anos = idade;
-    var meses = idade * 12;
+    var anos = idd;
+    var meses = idd * 12;
     var dias = meses * 30;
 
     console.log(anos + " ano(s)");
